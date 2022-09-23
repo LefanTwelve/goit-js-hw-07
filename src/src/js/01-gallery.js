abfcd9ed560a8c855311e971b9087f53d0ec8c2b
Aleksey Krupskyi <108680778+LefanTwelve@users.noreply.github.com>
2022-09-23 13:58:13 +0300

    Update

---
+++ src/src/js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+
+function createGalleryItems ({ preview, original, description}) {
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </div>
+    `;
+}
+
+const addGalleryItems = galleryItems.map(createGalleryItems).join('');
+galleryContainer.insertAdjacentHTML ('beforeend', addGalleryItems);
+
+
+galleryContainer.addEventListener("click", openModalPicture);
+
+function openModalPicture(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return
+    };
+
+    const instance = basicLightbox.create((`<img src="${event.target.getAttribute('data-source')}" width="800" height="600">`),
+    
+    {
+        onShow: () => {
+        document.addEventListener('keydown', onEscapeClose);
+    }, 
+
+       onClose: () => {
+        document.removeEventListener('keydown', onEscapeClose);
+    }
+    });
+
+    instance.show()
+
+    function onEscapeClose(event) {
+        if(event.key === "Escape") {
+            instance.close();
+        }
+    }
+};
---
+++ src/src/js/02-lightbox.js
@@ -0,0 +1,21 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItems ({ preview, original, description}) {
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </div>
+    `;
+}
+
+const addGalleryItems = galleryItems.map(createGalleryItems).join('');
+galleryContainer.insertAdjacentHTML ('beforeend', addGalleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, scrollZoom: true, captionsData: "alt" });
